b697bd2902d6985c4321421f7c1f58cde18f4348
abc132231cb <pereanusteven@gmail.com>
2023-03-01 15:59:25 -0500

    fixed some errors

--- src/shared/Tile.java
+++ src/shared/Tile.java
@@ -14,8 +14,8 @@ public class Tile implements Serializable {
     boolean change = true;
 
     public Tile(int column, int row, int current_index, int lower_index, int upper_index, boolean change) {
-        this.x = column * StaticOptions.CHUNKSIZE;
-        this.y = row * StaticOptions.CHUNKSIZE;
+        this.x = column * Options.TILE_SIZE;
+        this.y = row * Options.TILE_SIZE;
         this.lower_index = lower_index;
         this.upper_index = upper_index;
         this.current_index = current_index;
@@ -25,7 +25,7 @@ public class Tile implements Serializable {
 
     public void draw(Graphics g, int x, int y) {
         if (this.x -x + Options.TILE_SIZE > 0 && this.y-y + Options.TILE_SIZE >0 && (this.y-y) < Options.GAME_HEIGHT && this.x-x < Options.GAME_WIDTH)
-            g.drawImage(StaticOptions.TextureAtlas[current_index], (this.x-x), (this.y-y), StaticOptions.CHUNKSIZE, StaticOptions.CHUNKSIZE, null);
+            g.drawImage(StaticOptions.TextureAtlas[current_index], (this.x-x), (this.y-y), Options.TILE_SIZE, Options.TILE_SIZE, null);
     }
 
     public void changeState(boolean change, int lower_index, int upper_index) {
